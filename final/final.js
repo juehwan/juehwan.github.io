8050640f33a157a019483c2c46bf1a32b38eb765
bg0077 <bg0077@naver.com>
2018-06-13 13:39:32 +0900

    마지막

---
+++ final/final.js
@@ -0,0 +1,16 @@
+/*
+$(document).ready(function() {
+          $('#main #menu_oron a').click(function(){
+                      var string=$(this).attr('class');
+                      var strArray=string.split('_');
+                      $('.con_box >div').hide();
+                      $('.con_box .content_'+strArray[1]).show();
+                      $('.con_box .content_'+strArray[1]).innerHTML="test";
+      })
+ })
+ 
+ function first(){
+    var content1 = document.getElementById("content_1");
+    content1.innerHTML="test";
+ }
+ */
